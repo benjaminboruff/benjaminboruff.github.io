2879aed4fe0bfef1f70be2599d54b3e1462a7523
Benjamin Boruff <benboruff@gmail.com>
2015-03-26 20:13:57 -0400

    finished angular snippet/selections

--- js/common.js
+++ js/common.js
@@ -3,7 +3,28 @@ angular.module('common',[])
   .directive('commonNav', function () {
     return {
       restrict: 'E',
-      templateUrl: 'site-nav.html'
+      templateUrl: 'site-nav.html',
+      controller: function ($location) {// start nav button Controller
+        var currentLocation = $location.absUrl().split("/")[3];
+        // set nav button as active depending on html file name/location 
+        if (currentLocation === 'index.html') {
+          this.navBtn = 1; 
+        } else if (currentLocation === 'about.html') {
+          this.navBtn = 2;
+        } else if (currentLocation === 'contact.html') {
+          this.navBtn = 3;
+        } else {
+          this.navBtn = 1;
+        }
+
+        this.selectBtn = function (setNavBtn) {
+          this.navBtn = setNavBtn;
+        };
+        this.isSelected = function (checkNavBtn) {
+          return this.navBtn === checkNavBtn;
+        };
+      },// End Panel Controller
+      controllerAs: 'navtop'
     };
   })
   .directive('commonFooter', function () {
