1643533350b1e035fa204f17a5a709a80155faff
Benjamin Boruff <benboruff@gmail.com>
2015-04-15 16:36:07 -0400

    edited gulpfile to setup dev dir

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,18 @@
 var gulp = require('gulp');
 // Define source and build directories
 var src = 'dev/';
-var build = 'build/';
+var build = '/';
+// mkdev directories
+var devsrc = 'public/vendor/';
+var = jqsrc = 'jquery/dist/jquery.js';
+var = bootjssrc = 'bootstrap/dist/js/bootstrap.js';
+var = bootcsssrc = 'bootstrap/dist/css/bootstrap.css';
+var = bootcssthsrc = 'bootstrap/dist/css/bootstrap-theme.css';
+var = angularsrc = 'angular/angular.js';
+var = angularrtsrc = 'angular-route/angular-route.js';
+var devdest = 'dev/';
+
+
 // Include plug-ins
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
@@ -21,6 +32,11 @@ gulp.task('scripts', function () {
 gulp.task('index', function () {
   return gulp.src(src + 'index.html')
     .pipe(gulp.dest(build));
+});
+
+// Setup Angular dev directories in main project directory
+gulp.task('mkdevng', function () {
+  return gulp.src(devsrc +)
 });
 
 // Default task
