5d6e2a0598467a2747a8e358451750e18015e28c
Benjamin Boruff <benboruff@gmail.com>
2015-06-04 19:28:14 -0400

    turned off console error logging

--- dev/js/app.js
+++ dev/js/app.js
@@ -24,7 +24,7 @@
     }])
     .service('apodService', ['$http', function ($http) {
       this.get = function() {
-        console.log('Hello, from apodService!');
+        //console.log('Hello, from apodService!');
         return $http.get('https://api.data.gov/nasa/planetary/apod?api_key=ep9KoIgbcqVVVQ7JiLz5vcYql67pTJRPBTeeoIal&format=JSON')
           .then(function(res) {
             return res.data;
@@ -37,7 +37,7 @@
         apodService.get()
           .then(function(apodData){
             self.apodData = apodData;
-            console.log(self.apodData);
+            //console.log(self.apodData);
           });
       };
       self.getApodData();
