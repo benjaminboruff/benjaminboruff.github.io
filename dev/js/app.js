a7edb5ee6f921cac5408d87c264abe1322e75fe4
Benjamin Boruff <benboruff@gmail.com>
2015-06-04 19:19:49 -0400

    added APOD to jumbotron, and updated resume

--- dev/js/app.js
+++ dev/js/app.js
@@ -5,7 +5,9 @@
     .config(['$routeProvider', function ($routeProvider) {
       $routeProvider.
         when ('/portfolio', {
-          templateUrl: 'partials/portfolio.html'
+          templateUrl: 'partials/portfolio.html',
+          controller: 'PortfolioCtrl',
+          controllerAs: 'pfctrl'
         }).
         when ('/resume', {
           templateUrl: 'partials/resume.html'
@@ -19,5 +21,25 @@
         otherwise ({
           redirectTo: '/portfolio'
         });
+    }])
+    .service('apodService', ['$http', function ($http) {
+      this.get = function() {
+        console.log('Hello, from apodService!');
+        return $http.get('https://api.data.gov/nasa/planetary/apod?api_key=ep9KoIgbcqVVVQ7JiLz5vcYql67pTJRPBTeeoIal&format=JSON')
+          .then(function(res) {
+            return res.data;
+          });
+      };
+    }])
+    .controller('PortfolioCtrl', ['apodService', function (apodService) {
+      var self = this;
+      self.getApodData = function(){
+        apodService.get()
+          .then(function(apodData){
+            self.apodData = apodData;
+            console.log(self.apodData);
+          });
+      };
+      self.getApodData();
     }]);
 })();
