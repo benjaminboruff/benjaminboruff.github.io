d28e227583bf93a1540b67e238f71627cfdb28f8
Benjamin Boruff <benboruff@gmail.com>
2015-06-10 02:03:21 -0400

    fixed media_type detection for APOD

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,45 +1,56 @@
+/// <reference path="../typings/angularjs/angular.d.ts"/>
 "use strict";
 // Site manager app
 (function () {
-  angular.module('site-manager',['ngRoute', 'ui.bootstrap'])
+  angular.module('site-manager', ['ngRoute', 'ui.bootstrap'])
     .config(['$routeProvider', function ($routeProvider) {
-      $routeProvider.
-        when ('/portfolio', {
-          templateUrl: 'partials/portfolio.html',
-          controller: 'PortfolioCtrl',
-          controllerAs: 'pfctrl'
-        }).
-        when ('/resume', {
-          templateUrl: 'partials/resume.html'
-        }).
-        when ('/about', {
-          templateUrl: 'partials/about.html'
-        }).
-        when ('/contact', {
-          templateUrl: 'partials/contact.html'
-        }).
-        otherwise ({
-          redirectTo: '/portfolio'
-        });
-    }])
+    $routeProvider.
+      when('/portfolio', {
+      templateUrl: 'partials/portfolio.html',
+      controller: 'PortfolioCtrl',
+      controllerAs: 'pfctrl'
+    }).
+      when('/resume', {
+      templateUrl: 'partials/resume.html'
+    }).
+      when('/about', {
+      templateUrl: 'partials/about.html'
+    }).
+      when('/contact', {
+      templateUrl: 'partials/contact.html'
+    }).
+      otherwise({
+      redirectTo: '/portfolio'
+    });
+  }])
     .service('apodService', ['$http', function ($http) {
-      this.get = function() {
-        //console.log('Hello, from apodService!');
-        return $http.get('https://api.data.gov/nasa/planetary/apod?api_key=ep9KoIgbcqVVVQ7JiLz5vcYql67pTJRPBTeeoIal&format=JSON')
-          .then(function(res) {
-            return res.data;
-          });
-      };
-    }])
+    this.get = function () {
+      //console.log('Hello, from apodService!');
+      return $http.get('https://api.data.gov/nasa/planetary/apod?api_key=ep9KoIgbcqVVVQ7JiLz5vcYql67pTJRPBTeeoIal&format=JSON')
+        .then(function (res) {
+        return res.data;
+      });
+    };
+  }])
     .controller('PortfolioCtrl', ['apodService', function (apodService) {
-      var self = this;
-      self.getApodData = function(){
-        apodService.get()
-          .then(function(apodData){
-            self.apodData = apodData;
-            //console.log(self.apodData);
-          });
-      };
-      self.getApodData();
-    }]);
+    var self = this;
+    self.getApodData = function () {
+      apodService.get()
+        .then(function (apodData) {
+        self.apodData = apodData;
+        self.video = false;
+        if (self.apodData.media_type === 'video') {
+          self.video = true;
+        }
+        //console.log(self.video);
+        //console.log(self.apodData.url);
+      });
+    };
+    self.getApodData();
+  }])
+    .filter('trusted', ['$sce', function ($sce) {
+    return function (url) {
+      return $sce.trustAsResourceUrl(url);
+    };
+  }]);
 })();
