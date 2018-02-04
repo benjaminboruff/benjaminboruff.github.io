2a8a55743f06bdce6a113e3095c9384ae8a4b796
Benjamin H Boruff <ben@boruff.me>
2018-02-04 14:18:07 -0500

    moves site into ganstby-config to be accessed via gql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Benjamin H Boruff',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,10 +4,10 @@ import Helmet from 'react-helmet'
 import Header from '../components/Header'
 // import './index.css'
 
-const TemplateWrapper = ({ children }) => (
+const TemplateWrapper = ({ children, data }) => (
   <div>
     <Helmet
-      title="Benjamin H Boruff"
+      title={data.site.siteMetadata.title}
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
@@ -34,3 +34,13 @@ TemplateWrapper.propTypes = {
 }
 
 export default TemplateWrapper
+
+export const query = graphql`
+  query TitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
